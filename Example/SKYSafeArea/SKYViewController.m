ad613d9bcc906c8f7041a92440d9b9b5d43505e0
Wei Sun <sunw@fxiaoke.com>
2017-11-05 16:20:43 +0800

    done testing sky_safeBounds.

--- Example/SKYSafeArea/SKYViewController.m
+++ Example/SKYSafeArea/SKYViewController.m
@@ -8,6 +8,8 @@
 
 #import "SKYViewController.h"
 
+#import "UIViewController+safeArea.h"
+
 @interface SKYViewController ()
 
 @end
@@ -24,6 +26,11 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    CGRect safeBounds = self.sky_safeBounds;
+    NSLog(@"a breakpoint");
 }
 
 @end
--- SKYSafeArea/Classes/UIViewController+safeArea.m
+++ SKYSafeArea/Classes/UIViewController+safeArea.m
@@ -10,8 +10,27 @@
 
 #import "ObjcAssociatedObjectHelpers.h"
 
+#import "Aspects.h"
+
 @implementation UIViewController (safeArea)
 
 SYNTHESIZE_ASC_PRIMITIVE(sky_safeBounds, setSky_safeBounds, CGRect);
+
+
++ (void)load {
+    [self aspect_hookSelector:@selector(viewWillLayoutSubviews) withOptions:AspectPositionBefore usingBlock:^(id<AspectInfo> aspectInfo) {
+//should be called inside viewWillLayoutSubviews
+
+        UIViewController *viewController = (UIViewController *) [aspectInfo instance];
+
+        CGFloat bottomSafeGap = @available(iOS 11, *) ? viewController.view.safeAreaInsets.bottom : viewController.bottomLayoutGuide.length;
+
+        CGRect bounds = viewController.view.bounds;
+        bounds.size.height -= bottomSafeGap;
+
+        viewController.sky_safeBounds = bounds;
+
+    } error:nil];
+}
 
 @end
