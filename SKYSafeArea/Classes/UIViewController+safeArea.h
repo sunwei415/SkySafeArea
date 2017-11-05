1ecb989e67ecc9f8ff699a7e6b26280617e0fe2c
Wei Sun <sunw@fxiaoke.com>
2017-11-05 15:56:19 +0800

    adding category on UIViewController.

---
+++ SKYSafeArea/Classes/UIViewController+safeArea.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+safeArea.h
+//  Pods
+//
+//  Created by Wei Sun on 05/11/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (safeArea)
+
+@end
---
+++ SKYSafeArea/Classes/UIViewController+safeArea.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+safeArea.m
+//  Pods
+//
+//  Created by Wei Sun on 05/11/2017.
+//
+//
+
+#import "UIViewController+safeArea.h"
+
+@implementation UIViewController (safeArea)
+
+@end
